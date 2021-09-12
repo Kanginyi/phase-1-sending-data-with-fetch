f6f9c8649f261c92c7e38016bc1d5e228f016220
Eric Yi <erickanginyi@gmail.com>
2021-09-11 22:38:11 -0400

    commit tree d70867ee8bd435d454f8bff7ea9dc4313a86180d

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const BASE_URL = "http://localhost:3000/users";
+
+const submitData = (userName, userEmail) => {
+    return fetch(BASE_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        document.body.innerHTML = data.id;
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message;
+    })
+}
+
